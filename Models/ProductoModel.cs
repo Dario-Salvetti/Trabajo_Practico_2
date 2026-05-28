812ad556d755ebad127b5d64053def014faa3168
Ivana <ivanaceciliaschwindt@gmail.com>
2026-05-28 16:33:15 -0300

    rama1creada y clase producto

---
+++ Models/ProductoModel.cs
@@ -0,0 +1,7 @@
+public class Producto // los pongo public? 
+{
+    public int Id {get; set;}
+    public string Nombre {get; set;}
+    public int stock {get; set;}
+    public bool Perecedero {get; set;}
+} 
