dbdfa07a9cedeaf322058e6a615e8ca6d1a93e30
Dario_Salvetti <salvettidario@outlook.com.ar>
2026-06-13 16:33:53 -0300

    pequeña refactorizacion y agregado de servicios

--- Services/ProductoService.cs
+++ Services/ProductoService.cs
@@ -1,6 +1,7 @@
 using Microsoft.Data.Sqlite;
 using TP2.Models;
 using TP2.DTOs;
+using TP2.Services;
 
 namespace TP2.Services;
 
@@ -22,52 +23,95 @@ public class ProductoService
         comando.ExecuteNonQuery();
     }
 
-    public void CambiarPrecioXId(int id, int nprecio)
+    public ProductoIndividualDTO CambiarXId(int id, ProductoCambioDTO x)
     {
         using var conexion = new SqliteConnection($"Data Source={_rutaBaseDedatos}");
         conexion.Open();
         using var comando = conexion.CreateCommand();
 
-        comando.CommandText = "UPDATE Productos SET Precio = $precio WHERE Id = $id;";
+        if (x.Precio > 0)
+        {
+            comando.Parameters.Clear();
+            comando.CommandText = "UPDATE Productos SET Precio = $precio WHERE Id = $id;";
 
-        comando.Parameters.AddWithValue("$precio", nprecio);
-        comando.Parameters.AddWithValue("$id", id);
+            comando.Parameters.AddWithValue("$precio", x.Precio);
+            comando.Parameters.AddWithValue("$id", id);
 
-        comando.ExecuteNonQuery();
+            comando.ExecuteNonQuery();
+        }
+        
+        if (x.Stock != 0)
+        {
+            comando.Parameters.Clear();
+            comando.CommandText = "UPDATE Productos SET Stock = (Stock - $stock) WHERE Id = $id;";
+
+            comando.Parameters.AddWithValue("$stock", x.Stock);
+            comando.Parameters.AddWithValue("$id", id);
+
+            comando.ExecuteNonQuery();
+        }
+
+        if (x.Categoria > 0)
+        {
+            comando.Parameters.Clear();
+            comando.CommandText = "UPDATE Productos SET IdTipo = $cate WHERE Id = $id;";
+
+            comando.Parameters.AddWithValue("$cate", x.Categoria);
+            comando.Parameters.AddWithValue("$id", id);
+
+            comando.ExecuteNonQuery();
+        }
+
+        return ObtenerXId(id);
     }
 
-    public void CambiarStockXId(int id, int nstock)
+    public void BorrarXId (int id)
     {
         using var conexion = new SqliteConnection($"Data Source={_rutaBaseDedatos}");
         conexion.Open();
         using var comando = conexion.CreateCommand();
 
-        comando.CommandText = "UPDATE Productos SET Stock = (Stock - $stock) WHERE Id = $id;";
-
-        comando.Parameters.AddWithValue("$stock", nstock);
-        comando.Parameters.AddWithValue("$id", id);
-
+        comando.CommandText = "DELETE FROM Productos WHERE Id = $id;";
+        comando.Parameters.AddWithValue("$id",id);
         comando.ExecuteNonQuery();
     }
-
-    public void CambiarCategoriaXId(int id, int ncateg)
-    {
-        using var conexion = new SqliteConnection($"Data Source={_rutaBaseDedatos}");
-        conexion.Open();
-        using var comando = conexion.CreateCommand();
-
-        comando.CommandText = "UPDATE Productos SET IdTipo = $cate WHERE Id = $id;";
-
-        comando.Parameters.AddWithValue("$cate", ncateg);
-        comando.Parameters.AddWithValue("$id", id);
-
-        comando.ExecuteNonQuery();
-    }
-
     
+    public ProductoIndividualDTO ObtenerXId(int id)
+    {
+        
+        using var conexion = new SqliteConnection($"Data Source={_rutaBaseDedatos}");
+        conexion.Open();
+        using var comando = conexion.CreateCommand();
 
+        comando.CommandText = "SELECT Id, IdTipo, Nombre, Stock, Precio FROM Productos WHERE Id = $id;";
+
+        comando.Parameters.AddWithValue("$id", id);
+
+        using var leer = comando.ExecuteReader();
+
+        if (leer.Read())
+        {
+            return new ProductoIndividualDTO
+            {
+                Id = leer.GetInt32(0),
+                Nombre = leer.GetString(1),
+                Precio = leer.GetInt32(2),
+                Stock = leer.GetInt32(3),
+                Categoria = _catalogoService.ObtenerNombreXid(leer.GetInt32(4))
+            };
+        }
+
+        return null;
+    }
+
+    private readonly CatalogoService _catalogoService;
+
+    public ProductoService(CatalogoService catalogoService)
+    {
+         _catalogoService = catalogoService;
+    }
     //MODIFICAR LUEGO LO QUE ESTE DEBAJO DE ESTA LINEA
-    public List<Producto> GetAllProductos()
+    public List<Producto> GetAllProductos(int idt)
     {
         List<Producto> prod = new List<Producto>();
 
@@ -76,7 +120,9 @@ public class ProductoService
         conexion.Open();
 
         using var comando = conexion.CreateCommand();
-        comando.CommandText = "SELECT Id, IdTipo, Nombre, Stock, Precio FROM Productos;";
+        comando.CommandText = "SELECT Id, Nombre, Stock, Precio FROM Productos WHERE IdTipo = $idt;";
+
+        comando.Parameters.AddWithValue("$idt", idt);
         using var res = comando.ExecuteReader();
         
         while (res.Read())
@@ -93,31 +139,7 @@ public class ProductoService
 
         return prod;
     }
-    public Producto ObtenerXId(int id)
-    {
-        using var conexion = new SqliteConnection($"Data Source={_rutaBaseDedatos}");
-        conexion.Open();
-        using var comando = conexion.CreateCommand();
 
-        comando.CommandText = "SELECT Id, IdTipo, Nombre, Stock, Precio FROM Productos WHERE Id = $id;";
-
-        comando.Parameters.AddWithValue("$id", id);
-
-        using var leer = comando.ExecuteReader();
-
-        if (leer.Read())
-        {
-            return new Producto
-            {
-                Id = leer.GetInt32(0),
-                Nombre = leer.GetString(1),
-                Precio = leer.GetInt32(2),
-                Stock = leer.GetInt32(3),
-            };
-        }
-
-        return null;
-    }
 
     //
 }
